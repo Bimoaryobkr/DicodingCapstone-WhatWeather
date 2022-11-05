3b041008b02bde3e2bb95354315774a354433234
bimoaryob2707 <bimoaryob2707@gmail.com>
2022-11-05 18:49:12 +0700

    Instalasi React dan Persiapan File Project (1)

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PageNotFound() {
+    return (
+        <div className='error-message'>
+            <h1>Error Code: 404</h1>
+            <h1>Page Not Found</h1>
+        </div>
+    );
+}
+
+export default PageNotFound;
