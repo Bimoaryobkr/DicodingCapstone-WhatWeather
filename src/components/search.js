6dffb42fe12ceb4bb61dbf03431b9aacb62f07a5
bimoaryob2707 <bimoaryob2707@gmail.com>
2022-11-23 16:41:32 +0700

    CardWeather

--- src/components/search.js
+++ src/components/search.js
@@ -5,7 +5,7 @@ function SearchBar() {
         <div>
             <form className="d-flex search_bar my-4">
                 <input placeholder="Search" aria-label="Search" type="search" className="me-2 form-control" />
-                <button type="button" className="btn btn-primary">Search</button>
+                <button type="button" className="btn btn-dark">Search</button>
             </form>
         </div>
     )
