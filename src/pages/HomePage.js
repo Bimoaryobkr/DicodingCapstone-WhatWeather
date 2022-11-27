b63f057cb108ec1c7cb58a2d63c76df07e6ff042
Kallen <kelvinkur22@gmail.com>
2022-11-27 22:15:31 +0700

    Added state management for homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Search from '../components/search';
 import WeatherCard from '../components/weatherCard/WeatherCard';
+import getWeather from '../utils/dataSource';
 
 function HomePage() {
-  return (
-    <section className="container pt-3">
-      <div className="home_header border-bottom">
+  const [weathers, setWeathers] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [searchParam, setSearchParam] = useSearchParams();
+  const [cityKeyword, setCityKeyword] = React.useState(() => searchParam.get('city') || '');
+
+  const onSearch = (cityKeyword) => {
+    setCityKeyword(cityKeyword);
+    setSearchParam({ city: cityKeyword });
+  };
+
+  React.useEffect(() => {
+    const fetchWeather = async () => {
+      const weathersResponse = await getWeather();
+      setWeathers(weathersResponse);
+    }
+
+    fetchWeather().then(() => setLoading(false));
+  }, []);
+
+  let homepageContent = (
+    <section className='container pt-3'>
+      <div className='home_header border-bottom'>
         <h1>WhatWeather</h1>
       </div>
-      <div className="home_example mt-3">
+      <div className='home_example mt-3'>
         <h5>Cara melakukan pencarian</h5>
         <small>
           Silahkan melakukan pencarian dengan menggunakan nama kota. Untuk hasil
@@ -16,10 +37,14 @@ function HomePage() {
           Sebagai contoh Jakarta, ID.
         </small>
       </div>
-      <Search />
-      <WeatherCard />
+      <Search defaultKeyword={cityKeyword} onSearch={onSearch} />
+      <WeatherCard weathers={weathers} />
     </section>
-  )
+  );
+
+  return (
+    loading ? null : homepageContent
+  );
 }
 
 export default HomePage;
