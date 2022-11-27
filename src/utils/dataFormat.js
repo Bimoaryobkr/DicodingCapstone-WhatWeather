90de07d657cf83dc877989e66aa381298fa01e78
Kallen <kelvinkur22@gmail.com>
2022-11-27 22:14:26 +0700

    Added static methods to determine forecast range and format time

--- src/utils/dataFormat.js
+++ src/utils/dataFormat.js
@@ -1,14 +1,19 @@
 class DataFormat {
   static format(weatherData) {
-    const dateTimeData = this._formatDateTime(weatherData.timezone);
+    const dateTimeData = this._formatDateTime({
+      timezone: weatherData.timezone,
+      unixEpochTime: weatherData?.dt
+    });
 
     const weather = {
       city: weatherData.name || '',
       country: weatherData.sys.country || '',
+      timezone: weatherData.timezone,
+      dt: weatherData.dt,
       weatherInfo: {
         description: `${weatherData.weather[0].description[0].toUpperCase()}${weatherData.weather[0].description.slice(1)}`,
         feelsLike: weatherData.main.feels_like,
-        name: weatherData.weather[0].main.toLowerCase(),
+        name: weatherData.weather[0].main,
         time: dateTimeData.time,
         date: dateTimeData.date,
         temp: {
@@ -25,20 +30,31 @@ class DataFormat {
     return weather;
   }
 
-  static _formatDateTime(timezone) {
-    const currentLocaleTime = new Date();
-    currentLocaleTime.setHours((new Date().getHours() - 7) + (timezone / 3600));
+  static _formatDateTime({ timezone, unixEpochTime }) {
+    const currentLocaleTime = new Date(unixEpochTime * 1000);
 
-    const date = currentLocaleTime.toLocaleDateString('id-ID', { month: 'short', day: 'numeric' });
-    const time = currentLocaleTime
-      .toLocaleTimeString('id-ID', {
-        hourCycle: 'h24',
-        hour: 'numeric',
-        minute: 'numeric'
-      })
-      .replace('.', ':');
+    if (Boolean(timezone) !== false) {
+      currentLocaleTime.setHours((currentLocaleTime.getHours() - 7) + (timezone / 3600));
+    } else {
+      currentLocaleTime.setHours(currentLocaleTime.getHours() - 7);
+    }
+
+    const date = currentLocaleTime.toDateString();
+    const time = currentLocaleTime.toTimeString().slice(0, 5);
 
     return { time, date };
+  }
+
+  static determineTimeRange({ initialTimeUnixEpoch, forecastArray }) {
+    const forecastRange = [];
+    const initialTime = new Date(initialTimeUnixEpoch * 1000);
+
+    for (const hourlyData of forecastArray) {
+      const hourlyDate = new Date(hourlyData.dt_txt);
+      if ((hourlyDate > initialTime) && (forecastRange.length !== 3)) forecastRange.push(hourlyData);
+    }
+
+    return forecastRange;
   }
 }
 
