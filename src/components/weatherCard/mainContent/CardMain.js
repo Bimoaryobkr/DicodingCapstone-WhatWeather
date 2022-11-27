7d61a06f46aba175931adf9b29f5d959ba702eb6
Kallen <kelvinkur22@gmail.com>
2022-11-27 22:23:45 +0700

    Adjusted content displayed

--- src/components/weatherCard/mainContent/CardMain.js
+++ src/components/weatherCard/mainContent/CardMain.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { WiDayFog } from 'react-icons/wi';
 
-function CardMain() {
+function CardMain({ city, country, weatherInfo }) {
   return (
     <div className="col-md-6">
-      <h4 className="text-center mt-3">Jakarta</h4>
-      <p className="text-center time pb-3">ID, 09.00 WIB</p>
+      <h4 className="text-center mt-3">{city}</h4>
+      <p className="text-center time pb-3">{country}, {weatherInfo.time}</p>
       <div className="text-center">
         <WiDayFog className="weather_icon" size={150} />
       </div>
-      <h2 className="text-center mt-4 mb-4">Sunny</h2>
+      <h2 className="text-center mt-4 mb-4">{weatherInfo.name}</h2>
     </div>
   );
 }
