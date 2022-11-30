024680b1e6f492c0d14660ee6484d834fa0f0487
Kallen <kelvinkur22@gmail.com>
2022-11-30 13:39:06 +0700

    Moved homepage content to a var

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -29,22 +29,24 @@ function HomePage() {
     fetchWeather().then(() => setLoading(false));
   }, []);
 
+  const homepageContent = <section className='container pt-3'>
+    <div className='home_header border-bottom'>
+      <h1>WhatWeather</h1>
+    </div>
+    <div className='home_example mt-3'>
+      <h5>Cara melakukan pencarian</h5>
+      <small>
+        Silahkan melakukan pencarian dengan menggunakan nama kota. Untuk hasil
+        yang lebih tepat gunakan nama kota beri koma kemudian 2 kode negaranya.
+        Sebagai contoh Jakarta, ID.
+      </small>
+    </div>
+    <Search defaultKeyword={cityKeyword} refreshWeather={refreshWeather} onSearch={onSearch} />
+    <WeatherCard weathers={weathers} />
+  </section>;
+
   return (
-    loading ? null : <section className='container pt-3'>
-      <div className='home_header border-bottom'>
-        <h1>WhatWeather</h1>
-      </div>
-      <div className='home_example mt-3'>
-        <h5>Cara melakukan pencarian</h5>
-        <small>
-          Silahkan melakukan pencarian dengan menggunakan nama kota. Untuk hasil
-          yang lebih tepat gunakan nama kota beri koma kemudian 2 kode negaranya.
-          Sebagai contoh Jakarta, ID.
-        </small>
-      </div>
-      <Search defaultKeyword={cityKeyword} refreshWeather={refreshWeather} onSearch={onSearch} />
-      <WeatherCard weathers={weathers} />
-    </section>
+    loading ? null : homepageContent
   );
 }
 
