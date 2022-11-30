f20247ff07649e7337e93f83e7de4fb09ca66d11
Kallen <kelvinkur22@gmail.com>
2022-12-01 01:57:22 +0700

    Added detail chart component

---
+++ src/components/DetailChart.js
@@ -0,0 +1,56 @@
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+function DetailChart({ weatherData }) {
+  ChartJS.register(
+    LinearScale,
+    CategoryScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+    Legend,
+  );
+
+  const detailChartWrapperOnClick = (event) => {
+    event.stopPropagation();
+    event.target.classList.remove('detail-chart-wrapper--visible');
+
+    if (event.target.classList.contains('detail-chart-wrapper')) document.body.classList.remove('--disable-scroll');
+  };
+
+  const detailChartOptions = {
+    transitions: {
+      show: {
+        animations: {
+          x: { from: 0 },
+          y: { from: 0 },
+        },
+      },
+      hide: {
+        animations: {
+          x: { from: 0 },
+          y: { from: 0 },
+        },
+      },
+    },
+    maintainAspectRatio: false,
+  };
+
+  return (
+    <div onClick={detailChartWrapperOnClick} className='detail-chart-wrapper'>
+      <Line className='detail-chart' options={detailChartOptions} data={weatherData} />
+    </div>
+  );
+}
+
+export default DetailChart;
