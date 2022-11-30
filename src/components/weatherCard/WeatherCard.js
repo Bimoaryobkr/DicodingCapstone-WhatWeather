13982b94ba3280c5598667caba10200700ff8d45
Kallen <kelvinkur22@gmail.com>
2022-12-01 01:55:29 +0700

    Added DetailChart and its click event

--- src/components/weatherCard/WeatherCard.js
+++ src/components/weatherCard/WeatherCard.js
@@ -2,13 +2,23 @@ import React from 'react';
 import CardAside from './mainContent/CardAside';
 import CardMain from './mainContent/CardMain';
 import HourlyReport from './sideContent/HourlyReport';
+import DetailChart from '../DetailChart';
+import DataFormat from '../../utils/dataFormat';
 
 function WeatherCard({ weathers }) {
   const [primaryWeather, setPrimaryWeather] = React.useState(weathers[0]);
+  const weatherDataset = DataFormat.formatWeatherDataset(weathers);
 
   const onClick = (weatherDate) => {
     const selectedWeather = weathers.filter(weather => weather.dt === weatherDate)[0];
     setPrimaryWeather(selectedWeather);
+  };
+
+  const detailOnClick = (event) => {
+    event.stopPropagation();
+    const detailChart = document.querySelector('.detail-chart-wrapper');
+    detailChart.classList.add('detail-chart-wrapper--visible');
+    document.body.classList.add('--disable-scroll');
   };
 
   React.useEffect(() => {
@@ -28,7 +38,8 @@ function WeatherCard({ weathers }) {
       </div>
       <HourlyReport weathers={weathers} onClick={onClick} />
       <div className='text-center my-3'>
-        <button type='button' className='btn btn-dark'>Detail {'>>'}</button>
+        <button onClick={detailOnClick} type='button' className='btn btn-dark'>Detail {'>>'}</button>
+        <DetailChart weatherData={weatherDataset} />
       </div>
     </div>
 
