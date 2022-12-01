eb45b32172a88fba77c23404d636fa199c9cf727
bimoaryob2707 <bimoaryob2707@gmail.com>
2022-12-02 00:19:26 +0700

    Change to Https

--- src/components/weatherCard/sideContent/HourlyCard.js
+++ src/components/weatherCard/sideContent/HourlyCard.js
@@ -4,7 +4,7 @@ function HourlyCard({ weatherInfo, onClick }) {
   return (
     <div onClick={onClick} className='hourly-card col align-items-center justify-content-center text-center'>
       <p className='font-weight-light'>{weatherInfo.name}</p>
-      <img src={`http://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png`} alt="" />
+      <img src={`https://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png`} alt="" />
       <p>{weatherInfo.time}</p>
     </div>
   );
