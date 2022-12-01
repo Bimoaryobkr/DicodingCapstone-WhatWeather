bf49722e3ed3a6ae9859b711caf6e6389ac9c114
bimoaryob2707 <bimoaryob2707@gmail.com>
2022-12-01 23:36:56 +0700

    Change to HTTPS for Icon

--- src/components/weatherCard/mainContent/CardMain.js
+++ src/components/weatherCard/mainContent/CardMain.js
@@ -6,7 +6,7 @@ function CardMain({ city, country, weatherInfo }) {
       <h4 className="text-center mt-3">{city}</h4>
       <p className="text-center time pb-3">{country}, {weatherInfo.time}</p>
       <div className="text-center">
-        <img src={`http://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png`} alt="" />
+        <img src={`https://openweathermap.org/img/wn/${weatherInfo.icon}@2x.png`} alt="" />
       </div>
       <h2 className="text-center mt-4 mb-4">{weatherInfo.name}</h2>
     </div>
