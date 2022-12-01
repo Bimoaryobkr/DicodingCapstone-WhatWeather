614954f5a06b9c6fe7ee822baf87a2a41e677063
Kallen <kelvinkur22@gmail.com>
2022-12-01 21:52:37 +0700

    Changed ip API endpoint

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -1,7 +1,7 @@
 const API_ENDPOINTS = {
   WEATHER_API_KEY: '6995db48b84aa7cac68e95540c1bc36e',
-  DEFAULT_LOC: 'http://ip-api.com/json/',
-  TARGET_LOC: 'http://api.openweathermap.org/geo/1.0/direct',
+  DEFAULT_LOC: 'https://ipapi.co/json/',
+  TARGET_LOC: 'https://api.openweathermap.org/geo/1.0/direct',
   CURRENT_WEATHER: 'https://api.openweathermap.org/data/2.5/weather',
   PERIODIC_WEATHER: 'https://api.openweathermap.org/data/2.5/forecast',
 };
